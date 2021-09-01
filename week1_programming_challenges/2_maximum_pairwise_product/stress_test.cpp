3ac5fad9db38bf97741a637c93e82ad981e6718b
Shobuj Paul <shobuj.191ch048@nitk.edu.in>
2021-09-02 00:55:48 +0530

    max pairwise product

--- week1_programming_challenges/2_maximum_pairwise_product/stress_test.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/stress_test.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -33,11 +34,36 @@ long long MaxPairwiseProductFast(const std::vector<int>& numbers) {
         if(j!= max_index_1 && (max_index_2==-1 || numbers[j]>numbers[max_index_2]))
             max_index_2 = j;
     }
+
+    std::cout<<max_index_1<<' '<<max_index_2<<"\n";
     max_product = ((long long)numbers[max_index_1])*numbers[max_index_2];
     return max_product;
 }
 
 int main() {
+    while(true){
+        int n = rand() % 10 + 2;
+        std::cout<<n<<"\n";
+        std::vector<int> a;
+        for(int i = 0; i<n; i++){
+            a.push_back(rand() % 100000);
+        }
+        for(int i=0;i<n;i++){
+            std::cout<<a[i]<<' ';
+        }
+        std::cout<<"\n";
+        long long res1  =  MaxPairwiseProduct(a);
+        long long res2  =  MaxPairwiseProductFast(a);
+        if(res1!=res2){
+            std::cout<<"Wrong Answer: "<<res1<<' '<<res2<<"\n";
+            break;
+        }
+        else{
+            std::cout<<"OK\n";
+        }
+    }   
+
+
     int n;
     std::cin >> n;
     std::vector<int> numbers(n);
@@ -45,6 +71,5 @@ int main() {
         std::cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProductFast(numbers) << "\n";
     return 0;
 }
