2b89ac6fd1146dbd65545825e2b56983ba4a8808
Shobuj Paul <shobuj.191ch048@nitk.edu.in>
2021-09-05 20:16:48 +0530

    quick sort

--- week4_divide_and_conquer/4_improving_quicksort/sorting.cpp
+++ week4_divide_and_conquer/4_improving_quicksort/sorting.cpp
@@ -18,6 +18,34 @@ int partition2(vector<int> &a, int l, int r) {
   return j;
 }
 
+vector<int> partition3(vector<int> &a, int l, int r)
+{
+  int x = a[l];
+  int lt = l;
+  int gt = r;
+  int i = l;
+  while (i <= gt)
+  {
+    if (a[i] < x)
+    {
+      swap(a[i], a[lt]);
+      lt++;
+      i++;
+    }
+    else if (a[i] > x)
+    {
+      swap(a[i], a[gt]);
+      gt--;
+    }
+    else
+    {
+      i++;
+    }
+  }
+
+  return vector<int>{lt, gt};
+}
+
 void randomized_quick_sort(vector<int> &a, int l, int r) {
   if (l >= r) {
     return;
@@ -25,10 +53,14 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
-
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+//  int m = partition2(a, l, r);
+  vector<int> m = partition3(a,l,r);
+  int m1 = m[0];
+  int m2 = m[1];
+//  randomized_quick_sort(a, l, m - 1);
+//  randomized_quick_sort(a, m + 1, r);
+  randomized_quick_sort(a, l, m1 - 1);
+  randomized_quick_sort(a, m2 + 1, r);
 }
 
 int main() {
@@ -42,4 +74,5 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << ' ';
   }
+  std::cout<<"\n";
 }
