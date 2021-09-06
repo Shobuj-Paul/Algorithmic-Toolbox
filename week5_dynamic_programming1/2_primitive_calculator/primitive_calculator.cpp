b6a5620dee32438832cc48ed26421f1101e1dc01
Shobuj Paul <shobuj.191ch048@nitk.edu.in>
2021-09-07 02:26:47 +0530

    primitive calculator

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -4,6 +4,31 @@
 
 using std::vector;
 
+vector<int> dp_sequence(int n){
+  std::vector<int> sequence;
+  std::vector<int> arr(n+1);
+	for(int i=1; i< arr.size(); i++){
+		arr[i] = arr[i-1] + 1;
+		if (i%2 == 0) 
+			arr[i] = std::min(1 + arr[i/2], arr[i]);
+		if (i%3 == 0) 
+			arr[i] = std::min(1 + arr[i/3], arr[i]);
+	}
+	int i = n;
+	while (i>1) {
+		sequence.push_back(i);
+		if (arr[i-1] == arr[i]-1)
+			i = i-1;
+		else if (i%2 == 0 && (arr[i/2] == arr[i]-1))
+			i = i/2;
+		else if (i%3 == 0 && (arr[i/3] == arr[i]-1))
+			i = i/3;
+	}
+	sequence.push_back(1);
+	reverse(sequence.begin(), sequence.end());
+  return sequence;
+}
+
 vector<int> optimal_sequence(int n) {
   std::vector<int> sequence;
   while (n >= 1) {
@@ -23,9 +48,10 @@ vector<int> optimal_sequence(int n) {
 int main() {
   int n;
   std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
+  vector<int> sequence = dp_sequence(n);
   std::cout << sequence.size() - 1 << std::endl;
   for (size_t i = 0; i < sequence.size(); ++i) {
     std::cout << sequence[i] << " ";
   }
+  std::cout<<"\n";
 }
