a3e32d35e6988449a46a412189a3fef57a88790a
Shobuj Paul <shobuj.191ch048@nitk.edu.in>
2021-09-06 08:13:14 +0530

    week5

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -2,7 +2,23 @@
 
 int get_change(int m) {
   //write your code here
-  return m / 4;
+  int coins[] = {1,3,4};
+  int MinNumCoins[m+1];
+  MinNumCoins[0] = 0;
+  int NumCoins = 0;
+  for(int i=1;i<m+1;i++)
+  {
+    for(int j=0;j<3;j++)
+    {
+      if(i>=coins[j])
+      {
+        NumCoins = MinNumCoins[i-coins[j]] + 1;
+        if(NumCoins<MinNumCoins[i])
+          MinNumCoins[i] = NumCoins;
+      }
+    }
+  }  
+  return MinNumCoins[m];
 }
 
 int main() {
--- week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,19 @@
 # Uses python3
 def edit_distance(s, t):
     #write your code here
-    return 0
+    m, n = (len(s), len(t))
+    D = [[0 for i in range(m+1)] for j in range(n+1)]
+    for i in range(0,m+1):
+        for j in range(0,n+1):
+            if i==0:
+                D[i][j] == j
+            elif j==0:
+                D[i][j] == i
+            elif (s[i-1]==t[j-1]):
+                D[i][j] = D[i-1][j-1]
+            else:
+                D[i][j] = 1 + min(D[i][j-1],D[i-1][j],D[i-1][j-1])
+    return D[m][n]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
