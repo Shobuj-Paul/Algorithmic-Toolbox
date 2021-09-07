01e45cf6b5d6265f582bacef5b9b4c275ce7f65a
Shobuj Paul <shobuj.191ch048@nitk.edu.in>
2021-09-07 22:41:14 +0530

    week 6

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 using std::vector;
+using std::max;
 
 int optimal_weight(int W, const vector<int> &w) {
   //write your code here
@@ -14,6 +15,27 @@ int optimal_weight(int W, const vector<int> &w) {
   return current_weight;
 }
 
+int dp_optimal_weight(int W, const vector<int> &w)
+{
+  vector<vector<int>> values(w.size() + 1, vector<int>(W + 1));
+	for (size_t i = 0; i <= W; i++)
+    values[0][i] = 0;
+	for (size_t i = 0; i <= w.size(); i++)
+    values[i][0] = 0;
+
+	for (size_t i = 1; i <= w.size(); i++) {
+		for (size_t j = 1; j <= W; j++) {
+			if (j >= w[i - 1]) {
+				values[i][j] = max(w[i - 1] + values[i - 1][j - w[i - 1]],values[i - 1][j]);
+			} else {
+				values[i][j] = values[i - 1][j];
+			}
+		}
+	}
+
+	return values[w.size()][W];
+}
+
 int main() {
   int n, W;
   std::cin >> W >> n;
@@ -21,5 +43,5 @@ int main() {
   for (int i = 0; i < n; i++) {
     std::cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  std::cout << dp_optimal_weight(W, w) << '\n';
 }
